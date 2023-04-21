e386d53d259e89beed704d5f098d2f31acab66a2
camiquime98 <88402125+camiquime98@users.noreply.github.com>
2023-04-21 11:05:27 -0500

    Delete src directory

--- src/components/Image/Image.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Image = ({src, alt, classnames}) => (
-  <img src={src} alt={alt} className={classnames ? classnames : ''} />
-)
-
-export default Image;
--- src/components/NoInfo/NoInfo.components.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Noinfo = () => <span>N/A</span>;
-
-export default Noinfo;
